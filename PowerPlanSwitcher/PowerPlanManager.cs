e430b84f235d18dc6e1cfa54f133d4a462425aea
K900 <k0009000@gmail.com>
2013-03-26 20:14:29 +0400

    First commit
    Let's get started!

---
+++ PowerPlanSwitcher/PowerPlanManager.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace PowerPlanSwitcher
+{
+    /*
+     * This is a modified version of the source code originally found in
+     * https://github.com/dun3/Com.Hertkorn.SetPowerScheme
+     * I don't know what license that is under, so if the author has any issues,
+     * I'm willing to take it down and replace with my own implementation
+     * (even though there isn't too much to replace)
+     */
+
+    public struct PowerPlan
+    {
+        public string Name;
+        public Guid GUID;
+    }
+
+    public class PowerPlanManager
+    {        
+        public static PowerPlan Active {
+            get
+            {
+                IntPtr pCurrentSchemeGuid = IntPtr.Zero;
+                WinAPI.PowerGetActiveScheme(IntPtr.Zero, ref pCurrentSchemeGuid);
+                Guid currentSchemeGuid = (Guid)Marshal.PtrToStructure(pCurrentSchemeGuid, typeof(Guid));
+                return FindById(currentSchemeGuid);
+            }
+            set {
+                Guid schemeGuid = value.GUID;
+                WinAPI.PowerSetActiveScheme(IntPtr.Zero, ref schemeGuid);
+            }
+        }
+
+        public static IEnumerable<PowerPlan> FindAll()
+        {
+            Guid schemeGuid = Guid.Empty;
+
+            uint sizeSchemeGuid = (uint)Marshal.SizeOf(typeof(Guid));
+            uint schemeIndex = 0;
+
+            while (WinAPI.PowerEnumerate(IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, (uint)WinAPI.AccessFlags.ACCESS_SCHEME, schemeIndex, ref schemeGuid, ref sizeSchemeGuid) == 0)
+            {
+                string friendlyName = ReadFriendlyName(schemeGuid);
+
+                yield return new PowerPlan { Name = friendlyName, GUID = schemeGuid };
+
+                schemeIndex++;
+            }
+        }
+
+        public static PowerPlan FindById(Guid id)
+        {
+            IEnumerable<PowerPlan> plans = FindAll();
+            foreach (PowerPlan plan in plans) {
+                if (plan.GUID == id)
+                {
+                    return plan;
+                }
+            }
+            return new PowerPlan { };
+        }
+
+        private static string ReadFriendlyName(Guid schemeGuid)
+        {
+            uint sizeName = 1024;
+            IntPtr pSizeName = Marshal.AllocHGlobal((int)sizeName);
+
+            string friendlyName;
+
+            try
+            {
+                WinAPI.PowerReadFriendlyName(IntPtr.Zero, ref schemeGuid, IntPtr.Zero, IntPtr.Zero, pSizeName, ref sizeName);
+                friendlyName = Marshal.PtrToStringUni(pSizeName);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(pSizeName);
+            }
+
+            return friendlyName;
+        }
+    }
+
+    internal static class WinAPI
+    {
+        [DllImport("PowrProf.dll")]
+        public static extern UInt32 PowerEnumerate(IntPtr RootPowerKey, IntPtr SchemeGuid, IntPtr SubGroupOfPowerSettingGuid, UInt32 AcessFlags, UInt32 Index, ref Guid Buffer, ref UInt32 BufferSize);
+
+        public enum AccessFlags : uint
+        {
+            ACCESS_SCHEME = 16,
+            ACCESS_SUBGROUP = 17,
+            ACCESS_INDIVIDUAL_SETTING = 18
+        }
+
+        [DllImport("PowrProf.dll")]
+        public static extern UInt32 PowerReadFriendlyName(IntPtr RootPowerKey, ref Guid SchemeGuid, IntPtr SubGroupOfPowerSettingGuid, IntPtr PowerSettingGuid, IntPtr Buffer, ref UInt32 BufferSize);
+
+        [DllImport("PowrProf.dll")]
+        public static extern uint PowerGetActiveScheme(IntPtr UserRootPowerKey, ref IntPtr ActivePolicyGuid);
+
+        [DllImport("PowrProf.dll")]
+        public static extern uint PowerSetActiveScheme(IntPtr UserRootPowerKey, ref Guid SchemeGuid);
+    }
+}
---
+++ PowerPlanSwitcher/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace PowerPlanSwitcher
+{
+    static class Program
+    {
+        private static NotifyIcon trayIcon;
+
+        public static void updateMenu(ContextMenuStrip menu)
+        {
+            menu.Items.Clear();
+
+            foreach (PowerPlan plan in PowerPlanManager.FindAll())
+            {
+                ToolStripMenuItem item = new ToolStripMenuItem();
+                item.Text = plan.Name;
+                item.Tag = plan.GUID;
+                item.Checked = (plan.GUID == PowerPlanManager.Active.GUID);
+
+                item.Click += new EventHandler(delegate(object sender, EventArgs e)
+                {
+                    PowerPlanManager.Active = new PowerPlan { Name = null, GUID = (Guid)((ToolStripMenuItem)sender).Tag };
+                });
+
+                menu.Items.Add(item);
+            }
+
+            menu.Items.Add(new ToolStripSeparator());
+
+            ToolStripMenuItem exit = new ToolStripMenuItem();
+            exit.Text = "Exit";
+            exit.Click += new EventHandler(exitClicked);
+
+            menu.Items.Add(exit);
+        }
+
+        static void exitClicked(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+            trayIcon.Dispose();
+            Application.Exit();
+        }
+
+        static void Main()
+        {
+            // Set up icon
+            trayIcon = new NotifyIcon();
+            updateIcon();
+            trayIcon.MouseUp += new MouseEventHandler(trayIcon_MouseUp);
+
+            SystemEvents.PowerModeChanged += new PowerModeChangedEventHandler(SystemEvents_PowerModeChanged);
+
+            // Create menu
+            ContextMenuStrip menu = new ContextMenuStrip();
+            menu.Opening += new CancelEventHandler(menu_Opening);
+            updateMenu(menu);
+
+            // Add menu and start
+            trayIcon.ContextMenuStrip = menu;
+            trayIcon.Visible = true;
+            
+            Application.Run();
+        }
+
+        static void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e)
+        {
+            if (e.Mode == PowerModes.StatusChange)
+            {
+                updateIcon();
+            }
+        }
+
+        private static void updateIcon()
+        {
+            PowerStatus status = SystemInformation.PowerStatus;
+            bool online = (status.PowerLineStatus == PowerLineStatus.Online);
+            int percent = (int)Math.Round(status.BatteryLifePercent * 100);
+
+            Bitmap iconBitmap;
+            
+            if (percent < 20)
+            {
+                iconBitmap = online ? Resource1.gpm_battery_000_charging : Resource1.gpm_battery_000;
+            }
+            else if (percent < 40)
+            {
+                iconBitmap = online ? Resource1.gpm_battery_020_charging : Resource1.gpm_battery_020;
+            }
+            else if (percent < 60)
+            {
+                iconBitmap = online ? Resource1.gpm_battery_040_charging : Resource1.gpm_battery_040;
+            }
+            else if (percent < 80)
+            {
+                iconBitmap = online ? Resource1.gpm_battery_060_charging : Resource1.gpm_battery_060;
+            }
+            else if (percent < 100)
+            {
+                iconBitmap = online ? Resource1.gpm_battery_080_charging : Resource1.gpm_battery_080;
+            }
+            else
+            {
+                iconBitmap = online ? Resource1.gpm_battery_100_charging : Resource1.gpm_battery_100;
+            }
+
+            trayIcon.Icon = Icon.FromHandle(iconBitmap.GetHicon());
+            trayIcon.Text = String.Format("Power profile switcher: {0}%", percent);
+        }
+
+        static void trayIcon_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                System.Reflection.MethodInfo mi = typeof(NotifyIcon).GetMethod("ShowContextMenu", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+                mi.Invoke(trayIcon, null);
+            };
+        }
+
+        static void menu_Opening(object sender, CancelEventArgs e)
+        {
+            updateMenu((ContextMenuStrip)sender);
+        }   
+    }
+}
